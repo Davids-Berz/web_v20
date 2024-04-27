a01c434365f612f4c2fa3553fc0f47e6be6af5bd
DavidBerz <davids.berz@gmail.com>
2024-04-27 01:35:53 -0600

    @if @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web_v20';
+
+
+  public usuarios : string[] = ['David', 'Dianey', 'Hilda'];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.usuarios = [];
+    }, 2000);
+  }
+  
+
 }
