fd1e45a7ff2c22c62d391508e66095ea70f72eee
DavidBerz <davids.berz@gmail.com>
2024-05-05 12:29:15 -0600

    clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {CounterComponent} from "./counter/counter.component";
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent,RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'web_v20';
-  subtitle: string = 'Contador con estado de session';
-  counter: number = 0;
-
-  public usuarios : string[] = ['David', 'Dianey', 'Hilda'];
-
-  ngOnInit(): void {
-      this.usuarios = [];
-      this.counter = +(sessionStorage.getItem('counter')!) || 0;
-  }
-
-  setCounter(counter : number): void{
-    this.counter = counter;
-  }
-
-
 }
