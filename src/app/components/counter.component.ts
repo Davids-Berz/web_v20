3c29918704332db95202fde6f0a1a4f1d638570c
DavidBerz <davids.berz@gmail.com>
2024-06-08 01:30:35 -0600

    creando el componente counter + tslib

---
+++ src/app/components/counter.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styles: ``
+})
+export class CounterComponent {
+
+}
