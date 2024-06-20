d3e24a050683646714ff2c717b49e5df2e8dd20b
DavidBerz <davids.berz@gmail.com>
2024-06-19 19:52:28 -0600

    usando ngrx para el estado del contador

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideState, provideStore} from '@ngrx/store';
+import {_counterReducer} from './store/items.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore({
+    counter: _counterReducer
+  })]
 };
--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {decrement, increment, reset} from '../store/items.action';
 
 @Component({
   selector: 'app-counter',
@@ -9,25 +11,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CounterComponent implements OnInit {
   title: string = 'Contador usando Redux';
-  counter!: number;
+  counter: number;
+
+  constructor(private store: Store<{counter: number}>) {
+    this.counter = 0;
+  }
 
   ngOnInit(): void {
-    this.counter = 0;
+    this.store.select('counter').subscribe(counter => this.counter = counter);
   }
 
 
   increment(): void {
-    this.counter++;
+    this.store.dispatch(increment())
     console.log('incrementando')
   }
 
   decrement(): void {
-    this.counter--;
+    this.store.dispatch(decrement())
     console.log('decrementando')
   }
 
   reset(): void {
-    this.counter = 0;
+    this.store.dispatch(reset())
     console.log('reset de contador')
   }
 
