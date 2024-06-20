6d28f4fe668a2b54b35c8ccab595301f342e660d
DavidBerz <davids.berz@gmail.com>
2024-06-19 18:54:06 -0600

    agregando componente counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component,  } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component,} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CounterComponent} from './components/counter.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,6 +7,28 @@ import {Component} from '@angular/core';
   templateUrl: './counter.component.html',
   styles: ``
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  title: string = 'Contador usando Redux';
+  counter!: number;
+
+  ngOnInit(): void {
+    this.counter = 0;
+  }
+
+
+  increment(): void {
+    this.counter++;
+    console.log('incrementando')
+  }
+
+  decrement(): void {
+    this.counter--;
+    console.log('decrementando')
+  }
+
+  reset(): void {
+    this.counter = 0;
+    console.log('reset de contador')
+  }
 
 }
