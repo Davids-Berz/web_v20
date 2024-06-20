56994367b819f9c9ba2580b4ce633beb28fdfd39
DavidBerz <davids.berz@gmail.com>
2024-06-19 20:08:08 -0600

    config provideStoreDevtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideState, provideStore} from '@ngrx/store';
 import {_counterReducer} from './store/items.reducer';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideStore({
     counter: _counterReducer
-  })]
+  }),
+  provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()})]
 };
